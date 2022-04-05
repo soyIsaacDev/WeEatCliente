8cd6769417e3784eede4c418308e9b89450db969
Isaac Borbon <borbonisaac@hotmail.com>
2022-04-04 20:29:45 -0700

    Handle Location Correctly and post it to database just on update

--- we-eat/src/Components/GeolocationMap/Geolocation.js
+++ we-eat/src/Components/GeolocationMap/Geolocation.js
@@ -1,76 +1,36 @@
-import React, { Component, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
 
-// Importing geolocated reducer function
-//import { geolocated } from "react-geolocated";
-import { addGeolocation } from "../../Actions/actions";
-import { addUbicacionRepartidor } from "../../Actions/postFunctions";
+ import { addUbicacionRepartidor } from "../../Actions/postFunctions";
 
 export default function Geolocation (){
-	const dispatch = useDispatch();
-    
-    if(navigator.geolocation){
-        
-        navigator.geolocation.watchPosition(
-            (position) => {
-                const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                }
-				addUbicacionRepartidor( {"ubicacion":pos, "RepartidorId":1} );  
-                console.log(pos)
-                addGeolocation(pos);
-            }
-        )
-        return <div> Localizacion</div>
-        
-    } else{
-		alert("Favor de activar la Localizacion")
-        /* return(<div>
-            Favor de activar la Localizacion
-        </div>) */
-    }
 
-}
+	function success(pos) {
+		const posicion = {
+			lat: pos.coords.latitude,
+			lng: pos.coords.longitude,
+		}
+		addUbicacionRepartidor( {"name":"Emilia","ubicacion":posicion} );
+		console.log(posicion)
+	};
 
+	function error(err) {
+		alert('ERROR(' + err.code + '): ' + err.message);
+	};
 
-class App extends Component {
-render() {
+	const options = {
+	enableHighAccuracy: false,
+	maximumAge:0
+	};
 
-	// Check geolocation supported in
-	// browser or not
-	return this.props.isGeolocationAvailable ? (
-
-	// Check location is enable in
-	// browser or not
-	this.props.isGeolocationEnabled ? (
-
-		// Check coordinates of current
-		// location is available or not
-        
-
-		this.props.coords ? (
-		<div>
-			<h3 style={{ color: "red" }}>
-			Current latitude and longitude of the user is
-			</h3>
-			<ul>
-			<li>latitude - {this.props.coords.latitude}</li>
-			<li>longitude - {this.props.coords.longitude}</li>
-			</ul>
+	
+	if ('geolocation' in navigator){
+		navigator.geolocation.watchPosition(success, error, options);
+		return <div>Ubicacion</div>
+	}
+	else {
+		return <div>
+			<p>Geolocalizacion no disponible en su navegador</p>
+			<p>Favor de cambiar de navegador</p>
 		</div>
-		) : (
-		<h1>Getting the location data</h1>
-		)
-	) : (
-		<h1>Please enable location on your browser</h1>
-	)
-	) : (
-	<h1>Please, update your or change the browser </h1>
-	);
+	}
 }
-}
-
-// Binding geolocated() reducer function to
-// App component, while exporting it
-//export default geolocated()(App);
