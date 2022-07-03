f48bb367171c6b19dd1dc3b6a81ced535cfe206d
Isaac Borbon <borbonisaac@hotmail.com>
2022-07-02 21:12:33 -0700

    Build Added

---
+++ we-eat/build/precache-manifest.3cc86ef0fb487e8e542973a38b34489a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "538390d85a3224ff67ae",
+    "url": "/static/css/main.53506480.chunk.css"
+  },
+  {
+    "revision": "538390d85a3224ff67ae",
+    "url": "/static/js/main.538390d8.chunk.js"
+  },
+  {
+    "revision": "dca74a035a2640b5084a",
+    "url": "/static/js/1.dca74a03.chunk.js"
+  },
+  {
+    "revision": "b634907a45cc323333f4",
+    "url": "/static/js/2.b634907a.chunk.js"
+  },
+  {
+    "revision": "688055b876109e83e5ce",
+    "url": "/static/js/runtime~main.688055b8.js"
+  },
+  {
+    "revision": "e509e35e6aa89120869b9220a198f9fd",
+    "url": "/index.html"
+  }
+];
