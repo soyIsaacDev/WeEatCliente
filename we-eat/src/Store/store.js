c001ffff92949884284b775eabf3f43846ff6e4c
Isaac Borbon <borbonisaac@hotmail.com>
2022-07-11 16:32:01 -0700

    Deleted Redux Devtools from Store

--- we-eat/src/Store/store.js
+++ we-eat/src/Store/store.js
@@ -7,8 +7,7 @@ import thunk from "redux-thunk";
 
 const store = createStore(
     rootReducer, 
-    compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk))
     );
 
 export default store;
