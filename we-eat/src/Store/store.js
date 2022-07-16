f2ac893b33014987007bddb2ee63065ea3368c6e
Isaac Borbon <borbonisaac@hotmail.com>
2022-07-15 18:57:37 -0700

    Disable Redux

--- we-eat/src/Store/store.js
+++ we-eat/src/Store/store.js
@@ -7,9 +7,9 @@ import thunk from "redux-thunk";
 
 const store = createStore(
     rootReducer, 
-    /* compose(applyMiddleware(thunk)) */
-    compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk))
+    /* compose(applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) */
 );
 
 export default store;
